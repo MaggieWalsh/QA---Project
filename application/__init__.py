18eca0576076edf574ee279c09d2e0b7c060de92
Maggie Walsh <miggieholland@gmail.com>
2022-11-10 15:50:44 +0000

    Add database

--- application/__init__.py
+++ application/__init__.py
@@ -6,5 +6,3 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 
 db = SQLAlchemy(app)
-
-from application import routes
--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask, render_template
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
