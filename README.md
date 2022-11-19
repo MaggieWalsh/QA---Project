5ad9abf7c90dca9dc0c716263ae8161b4b1a6500
Maggie Walsh <miggieholland@gmail.com>
2022-11-19 13:42:14 +0000

    Attempt to add script to spin up venv, encountering errors

---
+++ README.md
@@ -0,0 +1,3 @@
+
+To make the script executable with: chmod +x run.sh
+Run the script by entering ./run.sh
---
+++ run.sh
@@ -0,0 +1,5 @@
+# How to add a file to spin up the environment
+cd application/
+source venv/bin/activate
+cd ..
+pip3 install -r requirements.txt
